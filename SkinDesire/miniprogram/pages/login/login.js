efe88cd18197b7139c8156e946cbe20c0c8c6ef2
LP_ZX <2295140520@qq.com>
2019-05-22 22:40:05 +0800

    page login

--- SkinDesire/miniprogram/pages/login/login.js
+++ SkinDesire/miniprogram/pages/login/login.js
@@ -11,51 +11,26 @@ Page({
   },
 
   // 生命周期函数--监听页面加载
-  onLoad: function (options) {
+  onLoad: function(options) {
+		let that = this
     wx.getSetting({
       success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          wx.redirectTo({
-            url: '../index/index'
-          })
+        if (res.authSetting['scope.userInfo']) {         
+					wx.getUserInfo({
+						success: function(res) {
+							that.updateUserInfo(res.userInfo)
+						}
+					})				
+					this.redirectToIndex()
         }
       }
     })
-
   },
 
   bindGetUserInfo(e) {
     if (e.detail.userInfo) {
-      wx.cloud.callFunction({
-        name: 'login',
-        data: {},
-        success: res => {
-          app.globalData.openid = res.result.openid
-          userInfoCollection.where({
-            _openid: app.globalData.openid
-          })
-            .get()
-            .then(res => {
-              if (res.data.length == 0) {
-                userInfoCollection.add({
-                  data: e.detail.userInfo
-                })
-                // console.log('添加成功')
-              }else {
-                userInfoCollection.doc(res.data[0]._id).update({
-                  data: e.detail.userInfo
-                })
-              }
-            })
-          wx.redirectTo({
-            url: '../index/index',
-          })
-        },
-        fail: err => {
-          console.error('[云函数] [login] 调用失败', err)
-        },
-      })
-    }else {
+      this.addUserInfo(e.detail.userInfo)
+    } else {
       wx.showModal({
         title: '警告',
         content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
@@ -63,5 +38,56 @@ Page({
         confirmText: '返回授权'
       })
     }
-  }
+  },
+
+  addUserInfo(userInfo) {
+    wx.cloud.callFunction({
+      name: 'login',
+      success: res => {
+        app.globalData.openid = res.result.openid
+        userInfoCollection.where({
+            _openid: app.globalData.openid
+          })
+          .count()
+          .then(res => {
+            if (res.total == 0) {
+              userInfoCollection.add({
+                data: userInfo
+              })
+							// console.log('添加成功')
+							this.redirectToIndex()
+            }
+          })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
+  },
+  updateUserInfo(userInfo) {
+    wx.cloud.callFunction({
+      name: 'login',
+      success: res => {
+        app.globalData.openid = res.result.openid
+        userInfoCollection.where({
+					_openid: app.globalData.openid
+				}).get()
+					.then(res => {
+						userInfoCollection.doc(res.data[0]._id).update({
+							data: userInfo
+						})
+						// console.log('更新成功')
+					})
+			},
+			fail: err => {
+				console.error('[云函数] [login] 调用失败', err)
+			}
+    })
+  },
+
+	redirectToIndex(){
+		wx.redirectTo({
+			url: '../index/index',
+		})
+	}
 })
