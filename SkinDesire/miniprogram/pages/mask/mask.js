de72d7ed7d829042366967fbf8be6de035822b02
LP_ZX <2295140520@qq.com>
2019-06-18 19:30:50 +0800

    mask

--- SkinDesire/miniprogram/pages/mask/mask.js
+++ SkinDesire/miniprogram/pages/mask/mask.js
@@ -1,50 +1,48 @@
+const db = wx.cloud.database()
+const maskCollection = db.collection('mask')
+// const _ = db.command
+
 Page({
 
 	//页面的初始数据
 	data: {
-
+		mask: {}
 	},
 
 	//生命周期函数--监听页面加载
 	onLoad: function (options) {
+		// console.log(options)
+		const { _id } = options
+		
+		wx.showLoading({
+			title: '加载中...',
+		})
 
+		wx.getStorage({
+			key: 'mask',
+			success:res => {
+				console.log(res)
+				this.setData({
+					mask: res.data
+				})
+			},
+		})
+
+		// maskCollection.where({
+		// 	_id
+		// }).get()
+			// .then(res => {
+			// 	const { desc, imgUrl, pInfo, pName, price } = res.data[0]
+			// 	this.setData({
+			// 			mask: {
+			// 				desc, 
+			// 				imgUrl, 
+			// 				pInfo, 
+			// 				pName, 
+			// 				price
+			// 			}
+			// 		})
+			// })
+		wx.hideLoading()
 	},
-
-	//生命周期函数--监听页面初次渲染完成
-	onReady: function () {
-
-	},
-
-	//生命周期函数--监听页面显示
-	onShow: function () {
-
-	},
-
-	//生命周期函数--监听页面隐藏
-
-	onHide: function () {
-
-	},
-
-	//生命周期函数--监听页面卸载
-	onUnload: function () {
-
-	},
-
-	//页面相关事件处理函数--监听用户下拉动作
-
-	onPullDownRefresh: function () {
-
-	},
-
-	//页面上拉触底事件的处理函数
-	onReachBottom: function () {
-
-	},
-
-	// 用户点击右上角分享
-
-	onShareAppMessage: function () {
-
-	}
 })
